fb7a61b4dba434db317f7fca3b0d5ccdb04b24ed
Anbarsan P <arsan.anbu97@gmail.com>
2026-03-18 14:09:44 +0530

    Added history option

--- src/app/features/chat/chat/chat.component.ts
+++ src/app/features/chat/chat/chat.component.ts
@@ -11,6 +11,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 
+const MAX_HISTORY = 20;
+
 @Component({
   selector: 'app-chat',
   standalone: true,
@@ -29,14 +31,57 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
   styleUrl: './chat.component.scss',
 })
 export class ChatComponent {
-  message = '';
+  message: string = '';
+
+  history: string[] = [];
+  historyIndex: number = -1;
 
   constructor(public chatService: ChatbotService) {}
 
   send() {
-    if (!this.message.trim()) return;
+    const trimmedMsg = this.message.trim();
+    if (!trimmedMsg) {
+      return;
+    };
 
-    this.chatService.sendMessage(this.message);
+    if (this.history[0] !== trimmedMsg) {
+      this.history.unshift(trimmedMsg);
+    }
+    if (this.history.length > MAX_HISTORY) {
+      this.history.pop();
+    }
+
+    this.chatService.sendMessage(trimmedMsg);
+
+    this.historyIndex = -1;
     this.message = '';
+  }
+
+  handleKey(event: KeyboardEvent) {
+    if (event.key === 'ArrowUp') {
+      event.preventDefault();
+      this.navigateHistory(1);
+    } else if (event.key === 'ArrowDown') {
+      event.preventDefault();
+      this.navigateHistory(-1);
+    }
+  }
+
+  navigateHistory(direction: number) {
+    if (this.history.length === 0) return;
+
+    this.historyIndex += direction;
+
+    if (this.historyIndex >= this.history.length) {
+      this.historyIndex = this.history.length - 1;
+    } else if (this.historyIndex < -1) {
+      this.historyIndex = -1;
+    }
+
+    if (this.historyIndex === -1) {
+      this.message = '';
+    } else {
+      this.message = this.history[this.historyIndex];
+    }
   }
 }
